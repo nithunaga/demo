9f80e0c6e72a2e045d70289d7d7bff488b55eec2
nithyanagalakshmim <113795341+nithyanagalakshmim@users.noreply.github.com>
2024-03-22 12:48:22 +0530

    Update AppTest.java

--- src/test/java/Vlead/login/AppTest.java
+++ src/test/java/Vlead/login/AppTest.java
@@ -1,25 +1,22 @@
 package Vlead.login;
 
-import org.junit.Test;
+
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AppTest {
     @Test
     public void testGoogleSearch() {
-        // Set path to chromedriver executable
-       
+   
+    public static void Main(String[] args) throws InterruptedException, IOException {
+        
         WebDriver driver = new ChromeDriver();
 
-        // Open Google
-        driver.get("https://www.google.com");
-        System.out.println("google");
-
-        // Perform some actions
-        // For example, you can find elements, enter text, click buttons, etc.
-
-        // Close the browser
-        driver.quit();
-    }
+        driver.get("https://www.google.com/");
+        System.out.println("successful login");
 }
+}
+
 
