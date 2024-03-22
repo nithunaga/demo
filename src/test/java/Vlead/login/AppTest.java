d057e188389e3c3a80fab3d289516707e7b3f3a0
nithyanagalakshmim <113795341+nithyanagalakshmim@users.noreply.github.com>
2024-03-22 13:02:42 +0530

    Update AppTest.java

--- src/test/java/Vlead/login/AppTest.java
+++ src/test/java/Vlead/login/AppTest.java
@@ -1,21 +1,22 @@
 package Vlead.login;
 
-
 import java.io.IOException;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AppTest {
-     {
-   
-    public static void Main(String[] args) throws InterruptedException, IOException {
-        
-        WebDriver driver = new ChromeDriver();
 
+    public static void main(String[] args) throws InterruptedException, IOException {
+        // Set system property to Chrome driver path
+       System.setProperty("webdriver.gecko.driver", "C:\\Users\\nithya.m\\geckodriver-v0.34.0-win64\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        // Open Google website
         driver.get("https://www.google.com/");
-        System.out.println("successful login");
-}
-}
 
-
+        // Print a message
+        System.out.println("Successful login");
+        
+        // Close the browser
+        driver.quit();
+    }
+}
