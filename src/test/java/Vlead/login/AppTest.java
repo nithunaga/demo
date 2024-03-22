b34fd58e557002866a6cc25bc40def4bc06c7036
nithyanagalakshmim <113795341+nithyanagalakshmim@users.noreply.github.com>
2024-03-22 12:49:01 +0530

    Update AppTest.java

--- src/test/java/Vlead/login/AppTest.java
+++ src/test/java/Vlead/login/AppTest.java
@@ -7,8 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AppTest {
-    @Test
-    public void testGoogleSearch() {
+     {
    
     public static void Main(String[] args) throws InterruptedException, IOException {
         
